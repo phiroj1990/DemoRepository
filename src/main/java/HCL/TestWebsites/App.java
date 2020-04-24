421af33e6b47c7e07e3bd6410631fc267d1a69e0
phiroj1990 <phirojsahoo1990@gmail.com>
2020-04-24 10:20:17 +0530

    Code checkin - Test project

---
+++ src/main/java/HCL/TestWebsites/App.java
@@ -0,0 +1,13 @@
+package HCL.TestWebsites;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
